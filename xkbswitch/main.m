c8f5cc573f46200af77e41fb57ef23f3b09c79c6
Alexander Myshov <alexander.myshov@gmail.com>
2015-01-02 18:59:59 +0800

    Initial Commit

---
+++ xkbswitch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xkbswitch
+//
+//  Created by Александр Мышов on 02.01.15.
+//  Copyright (c) 2015 Alexander Myshov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
